464e92425fa6d837132eee7091372fb8ffec5381
gg5287 <c12075598@gmail.com>
2025-11-26 05:39:46 +0800

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapPin, UserCheck, Clock } from 'lucide-react';
+import { MapPin, UserCheck } from 'lucide-react';
 
 const Contact: React.FC = () => {
   return (
